dbcbc9c5dbdcf3d829ec2bd81e3a0b0a4286ce54
fruitfly1026 <fruitfly1026@gmail.com>
2019-01-10 10:55:22 -0800

    minor

--- benchmarks/test_scripts/run_pasta_tew_same.sh
+++ benchmarks/test_scripts/run_pasta_tew_same.sh
@@ -3,7 +3,7 @@
 declare -a s3tsrs=("vast-2015-mc1" "choa700k" "1998DARPA" "nell2" "freebase_music" "freebase_sampled" "nell1" "delicious")
 declare -a l3tsrs=("amazon-reviews" "patents" "reddit-2015")
 declare -a s4tsrs=("chicago-crime-comm-4d" "nips-4d" "enron-4d" "flickr-4d" "delicious-4d")
-declare -a test_tsr_names=("freebase_music" "freebase_sampled")
+declare -a test_tsr_names=("vast-2015-mc1")
 declare -a threads=("2" "4" "8" "16" "32")
 
 # Cori
@@ -20,8 +20,8 @@ do
 
 	# Sequetial code
 	dev_id=-2
-echo "./build/benchmarks/${prog_name} -X ${tsr_path}/${tsr_name}.tns -Y ${tsr_path}/${tsr_name}.tns -d ${dev_id} -c 0 > ${out_path}/${tsr_name}-${prog_name}-c0-seq.txtz"
-	# ./build/tests/mttkrp -i ${tsr_path}/${tsr_name}.tns -d ${dev_id} -r ${R} > ${out_path}/${tsr_name}-r${R}-seq.txt
+	echo "./build/benchmarks/${prog_name} -X ${tsr_path}/${tsr_name}.tns -Y ${tsr_path}/${tsr_name}.tns -d ${dev_id} -c 0 > ${out_path}/${tsr_name}-${prog_name}-c0-seq.txt"
+	./build/benchmarks/${prog_name} -X ${tsr_path}/${tsr_name}.tns -Y ${tsr_path}/${tsr_name}.tns -d ${dev_id} -c 0 > ${out_path}/${tsr_name}-${prog_name}-c0-seq.txt
 
 
 	# OpenMP code
@@ -30,7 +30,7 @@ echo "./build/benchmarks/${prog_name} -X ${tsr_path}/${tsr_name}.tns -Y ${tsr_pa
 	# for nt in ${threads[@]}
 	# do
 		echo "numactl --interleave=0-1 ./build/benchmarks/${prog_name} -X ${tsr_path}/${tsr_name}.tns -Y ${tsr_path}/${tsr_name}.tns -d ${dev_id} -c 0 > ${out_path}/${tsr_name}-${prog_name}-c0-t${nt}.txt"
-		# numactl --interleave=0-3 ./build/tests/mttkrp -i ${tsr_path}/${tsr_name}.tns -d ${dev_id} -r ${R} -t ${nt} > ${out_path}/${tsr_name}-r${R}-t${nt}.txt
+		numactl --interleave=0-1 ./build/benchmarks/${prog_name} -X ${tsr_path}/${tsr_name}.tns -Y ${tsr_path}/${tsr_name}.tns -d ${dev_id} -c 0 > ${out_path}/${tsr_name}-${prog_name}-c0-t${nt}.txt
 	# done
 
 done
