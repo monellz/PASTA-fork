d32db9e4769fdec761f0f59eec74797287232ed4
Jiajia Li <fruitfly1026@gmail.com>
2019-08-01 02:13:53 -0700

    upload ignored files

---
+++ benchmarks/test_scripts/run_pasta_all_coo.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "./run_pasta_all_coo tsr_path out_path nt gpu_dev_id machine_name"
+
+tsr_path=$1		# "${SCRATCH}/BIGTENSORS"
+out_path=$2		# "./timing-results"
+nt=$3			# 32
+gpu_dev_id=$4	# 0, 1, ...
+machine_name=$5	# dgx2, wingtip-bigmem2, bluesky
+
+echo "./run_pasta_all_coo ${tsr_path} ${out_path} ${nt} ${gpu_dev_id} ${machine_name}"
+echo
+
+script_path="./benchmarks/test_scripts"
+
+declare -a dev_ids=("-2" "-1" ${gpu_dev_id})
+declare -a modes=("3" "4")
+
+for id in "${dev_ids[@]}"
+do
+	for nmodes in "${modes[@]}"
+	do
+		apped="${tsr_path} ${out_path} ${nmodes} ${nt} ${id} ${machine_name}"
+
+		# TS
+		${script_path}/run_pasta_smul.sh ${apped}
+
+		# TEW-eq
+		./benchmarks/test_scripts/run_pasta_dadd_eq.sh ${apped}
+
+		# TTV
+		./benchmarks/test_scripts/run_pasta_ttv.sh ${apped}
+
+		# TTM
+		./benchmarks/test_scripts/run_pasta_ttm.sh ${apped}
+
+		# MTTKRP
+		./benchmarks/test_scripts/run_pasta_mttkrp.sh ${apped}
+
+	done
+done
